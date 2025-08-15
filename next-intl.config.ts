09f869b3cfcbf8675bb9efd4fd9e0032d66c7ce8
Khaled Fakharany <fakharany@MACBOOKPRO-21B5.station>
2025-08-15 14:05:14 +0100

    next-intl: remove old JS/TS configs

--- next-intl.config.ts
+++
@@ -1,9 +0,0 @@
-// next-intl.config.ts (root)
-const config = {
-  locales: ['en', 'ar'],
-  defaultLocale: 'en',
-  // Do not prefix the default locale in URLs
-  localePrefix: 'as-needed'
-} as const;
-
-export default config;
