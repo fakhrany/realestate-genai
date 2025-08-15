d09ffde22a6bbac596c3a4de4d39ff592367685a
Khaled Fakharany <fakharany@MACBOOKPRO-21B5.station>
2025-08-15 14:06:06 +0100

    next-intl: add ESM config (.mjs)

---
+++ next-intl.config.mjs
@@ -0,0 +1,9 @@
+// next-intl.config.mjs (root, ESM)
+const config = {
+  locales: ['en', 'ar'],
+  defaultLocale: 'en',
+  // Don't prefix the default locale
+  localePrefix: 'as-needed'
+};
+
+export default config;
